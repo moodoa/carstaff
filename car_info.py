e48808ab83de36da7ee492e4c374b9cae84185b7
Weed <39092862+moodoa@users.noreply.github.com>
2023-02-15 16:36:22 +0800

    <fix>update url

--- car_info.py
+++ car_info.py
@@ -7,21 +7,18 @@ from tqdm import tqdm
 class CARSTAFF:
     def __init__(self, service_name):
         self.service_name = service_name
-        self.url = "https://www.partslink24.com/p5jlr/extern/vehicle/"
+        self.url = "https://www.partslink24.com"
 
     def main(self):
         infos = pd.DataFrame(data={"model": "", "engine": "", "gear": ""}, index=[0])
         for model in tqdm(self._get_models()):
             model_name = model["values"]["name"]
-            model_code = model["id"]
-            engines = self._get_engines(model_code)
+            engines = self._get_engines(model["link"]["path"])
             for engine in engines:
                 if engine["values"]["code"] != "全部":
-                    engine_code = engine["id"]
                     engine_name = engine["values"]["description"]
-                    for gear in self._get_gears(engine_code, model_code):
+                    for gear in self._get_gears(engine["link"]["path"]):
                         if gear["values"]["code"] != "全部":
-                            gear_code = gear["id"]
                             gear_name = gear["values"]["description"]
                             infos.loc[len(infos.index)] = [
                                 model_name,
@@ -34,19 +31,19 @@ class CARSTAFF:
 
     def _get_models(self):
         models = requests.get(
-            f"{self.url}models?lang=zh-TW&serviceName={self.service_name}"
+            f"{self.url}/p5jlr/extern/vehicle/models?lang=zh-TW&serviceName={self.service_name}"
         ).json()
         return models["data"]["records"]
 
-    def _get_engines(self, model_code):
+    def _get_engines(self, engine_path):
         engines = requests.get(
-            f"{self.url}engines?lang=zh-TW&mdl={model_code}&serviceName={self.service_name}&upds=2023-02-03--15-13"
+            f"{self.url}{engine_path}"
         ).json()
         return engines["data"]["records"]
 
-    def _get_gears(self, engine_code, model_code):
+    def _get_gears(self, gear_path):
         gears = requests.get(
-            f"{self.url}gearboxes?avsEn={engine_code}&lang=zh-TW&mdl={model_code}&serviceName={self.service_name}&upds=2023-02-03--15-13"
+            f"{self.url}{gear_path}"
         ).json()
         return gears["data"]["records"]
 
